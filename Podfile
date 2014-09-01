a5f1522a9038b395e768d28fbd412893f93e2276
Noon, Christian <christian.noon@nike.com>
2014-08-31 20:09:11 -0700

    Added the Masonry 0.5.3 pod to the project.

---
+++ Podfile
@@ -0,0 +1,5 @@
+platform :ios, "8.0"
+
+target "Swift-Masonry" do
+  pod 'Masonry'
+end
---
+++ Pods/Pods-Swift-Masonry-Masonry-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Swift_Masonry_Masonry : NSObject
+@end
+@implementation PodsDummy_Pods_Swift_Masonry_Masonry
+@end
